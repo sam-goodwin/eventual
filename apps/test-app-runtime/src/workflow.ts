5c5161c572113dc47974367e4d4cf9de30b2ff67
Sam Sussman <sussmansa@gmail.com>
2022-11-09 10:28:33 -0600

    begin reentry impl

--- apps/test-app-runtime/src/workflow.ts
+++ apps/test-app-runtime/src/workflow.ts
@@ -1,1 +1,58 @@
-export default "TODO";
+import { SQSHandler, SQSRecord } from "aws-lambda";
+import { S3 } from "@aws-sdk/client-s3";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+const s3 = new S3({});
+const dynamo = new DynamoDB({});
+
+// TODO abstract to a workflow function in aws-runtime.
+export const workflow: SQSHandler = async (event) => {
+  if (event.Records.some((r) => !!r.attributes.MessageGroupId)) {
+    throw new Error("Expected SQS Records to contain fifo message id");
+  }
+
+  const eventsByExecutionId = event.Records.reduce(
+    (obj: Record<string, SQSRecord[]>, r) => ({
+      ...obj,
+      [r.attributes.MessageGroupId!]: [
+        ...(obj[r.attributes.MessageGroupId!] || []),
+        r,
+      ],
+    }),
+    {}
+  );
+
+  const executionIds = Object.keys(eventsByExecutionId);
+
+  console.log("Found execution ids: " + executionIds.join(", "));
+
+  await Promise.all(
+    Object.entries(eventsByExecutionId).map(async ([executionId, records]) =>
+      handleExecutionEvents(executionId, sqsRecordsToEvents(records))
+    )
+  );
+};
+
+function sqsRecordsToEvents(sqsRecords: SQSRecord[]): WorkflowEvent[] {
+  return sqsRecords.flatMagit statusp(sqsRecordToEvents);
+}
+
+function sqsRecordToEvents(sqsRecord: SQSRecord): WorkflowEvent[] {
+  const message = JSON.parse(sqsRecord.body) as SQSWorkflowEventMessage;
+
+  return message.events;
+}
+
+async function handleExecutionEvents(
+  executionId: string,
+  events: WorkflowEvent[]
+) {
+    // get current history from s3
+    // invoke workflow
+    // evaluate the workflow result, execute commands,  
+}
+
+interface WorkflowEvent {}
+interface SQSWorkflowEventMessage {
+  events: WorkflowEvent[];
+}
--- packages/@eventual/aws-cdk/src/workflow.ts
+++ packages/@eventual/aws-cdk/src/workflow.ts
@@ -1,6 +1,10 @@
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Runtime, Architecture } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { FifoThroughputLimit, Queue } from "aws-cdk-lib/aws-sqs";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { RemovalPolicy } from "aws-cdk-lib";
 
 export interface WorkflowProps {
   entry: string;
@@ -9,15 +13,40 @@ export interface WorkflowProps {
 // placeholder
 export class Workflow extends Construct {
   constructor(scope: Construct, id: string, props: WorkflowProps) {
-    super(scope, id);   
+    super(scope, id);
 
-    new NodejsFunction(this, 'workflowFunction', {
-        entry: props.entry,
-        runtime: Runtime.NODEJS_16_X,
-        architecture: Architecture.ARM_64,
-        bundling: {
-            mainFields: ['module', 'main']
-        }
-    })
+    // ExecutionHistoryBucket
+    const executionHistoryBucket = new Bucket(this, "executionHistoryBucket");
+
+    // WorkflowQueue
+    const workflowQueue = new Queue(this, "workflowQueue", {
+      fifo: true,
+      fifoThroughputLimit: FifoThroughputLimit.PER_MESSAGE_GROUP_ID,
+    });
+
+    // Table - History, Execution, ExecutionData
+    const table = new Table(this, "table", {
+      partitionKey: { name: "pk", type: AttributeType.STRING },
+      sortKey: { name: "sk", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // workflow lambda
+    new NodejsFunction(this, "workflowFunction", {
+      entry: props.entry,
+      runtime: Runtime.NODEJS_16_X,
+      architecture: Architecture.ARM_64,
+      bundling: {
+        mainFields: ["module", "main"],
+      },
+      environment: {
+        TABLE_NAME: table.tableName,
+        WORKFLOW_QUEUE_ARN: workflowQueue.queueArn,
+        EXECUTION_HISTORY_BUCKET: executionHistoryBucket.bucketArn,
+      },
+    });
+
+    // TODO - timers and retry
   }
 }
