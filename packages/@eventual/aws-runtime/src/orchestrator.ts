7cbf8ecf4e566953500fa7c8d3109ca7a081dfd3
Sam Sussman <sussmansa@gmail.com>
2022-11-10 16:30:02 -0600

    more workflow

--- packages/@eventual/aws-runtime/src/orchestrator.ts
+++ packages/@eventual/aws-runtime/src/orchestrator.ts
@@ -1,27 +1,25 @@
-// import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-// import { S3Client } from "@aws-sdk/client-s3";
-// import { SQSClient } from "@aws-sdk/client-sqs";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { S3Client } from "@aws-sdk/client-s3";
 import type lambda from "aws-lambda";
-// import { executionHistoryBucket, tableName } from "./env";
-// import { ExecutionHistoryClient } from "./execution-history-client";
-// import { WorkflowRuntimeClient } from "./workflow-runtime-client";
+import { executionHistoryBucket, tableName } from "./env";
+import { ExecutionHistoryClient } from "./execution-history-client";
+import { WorkflowRuntimeClient } from "./workflow-runtime-client";
 import { Event, executeWorkflow } from "@eventual/core";
 
-// const s3 = new S3Client({});
-// const dynamo = new DynamoDBClient({});
-// const _sqs = new SQSClient({});
+const s3 = new S3Client({});
+const dynamo = new DynamoDBClient({});
 
-// const workflowRuntimeClient = new WorkflowRuntimeClient({
-//   dynamo,
-//   s3,
-//   // todo fail when missing
-//   executionHistoryBucket: executionHistoryBucket ?? "",
-//   tableName: tableName ?? "",
-// });
-// const executionHistoryClient = new ExecutionHistoryClient({
-//   dynamo,
-//   tableName: tableName ?? "",
-// });
+const workflowRuntimeClient = new WorkflowRuntimeClient({
+  dynamo,
+  s3,
+  // todo fail when missing
+  executionHistoryBucket: executionHistoryBucket ?? "",
+  tableName: tableName ?? "",
+});
+const executionHistoryClient = new ExecutionHistoryClient({
+  dynamo,
+  tableName: tableName ?? "",
+});
 
 /**
  * Creates an entrypoint function for orchestrating a workflow.
@@ -40,12 +38,11 @@ export function orchestrator(
     // execution commands
     // update history from new commands and events
   };
-}
 
-export async function orchestrateExecution(
-  _executionId: string,
-  _events: Event[]
-) {
-  // const history = await workflowRuntimeClient.getHistory(executionId);
-  // const allEvents = [...history, ...events];
+  async function orchestrateExecution(_executionId: string, _events: Event[]) {
+    const history = await workflowRuntimeClient.getHistory(executionId);
+    const allEvents = [...history, ...events];
+
+    executeWorkflow(program(null), { threads: [] });
+  }
 }
