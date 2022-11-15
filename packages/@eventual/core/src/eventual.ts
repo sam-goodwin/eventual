cf8b17ab59e0a607fd018f6d4b4c7d922a151432
sam <sam@functionless.org>
2022-11-14 16:10:47 -0800

    chore: rename Future to Eventual

--- packages/@eventual/core/src/eventual.ts
+++ packages/@eventual/core/src/eventual.ts
@@ -1,6 +1,7 @@
-import { Future } from "./future";
 import { Program } from "./interpret";
 import { registerChain, Chain } from "./chain";
+import { ActivityCall } from "./activity-call";
+import { AwaitAll } from "./await-all";
 
 export function eventual<F extends (...args: any[]) => Promise<any>>(
   func: F
@@ -19,6 +20,48 @@ export function eventual<F extends (...args: any[]) => any>(func: F): F {
 
 type Resolved<T> = T extends Program<infer U>
   ? Resolved<U>
-  : T extends Future<infer U>
+  : T extends Eventual<infer U>
   ? Resolved<U>
   : T;
+
+export const EventualSymbol = Symbol.for("eventual:Eventual");
+
+export enum EventualKind {
+  AwaitAll = 0,
+  ActivityCall = 1,
+  Chain = 2,
+}
+
+export function isEventual(a: any): a is Eventual {
+  return a && typeof a === "object" && EventualSymbol in a;
+}
+
+// rename: Future/Eventual/Awaitable
+export type Eventual<T = any> =
+  | ActivityCall<T>
+  | AwaitAll<T extends any[] ? T : never>
+  | Chain<T>;
+
+export namespace Future {
+  /**
+   * Wait for all {@link activities} to complete or until at least one throws.
+   *
+   * This is the equivalent behavior to Promise.all.
+   */
+  export function* all<A extends Eventual[]>(
+    activities: A
+  ): Program<
+    AwaitAll<{
+      [i in keyof A]: A[i] extends Eventual<infer T> ? T : A[i];
+    }>
+  > {
+    return (yield <
+      AwaitAll<{
+        [i in keyof A]: A[i] extends Eventual<infer T> ? T : A[i];
+      }>
+    >{
+      [EventualSymbol]: EventualKind.AwaitAll,
+      activities,
+    }) as any;
+  }
+}
