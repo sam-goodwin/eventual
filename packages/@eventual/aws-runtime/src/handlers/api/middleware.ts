1f5b1ef629893db8d1f3e86dc57ae89311b00d0f
Chris Fraser <chris@functionless.com>
2022-11-22 18:35:02 +1100

    Add middleware documentation

--- packages/@eventual/aws-runtime/src/handlers/api/middleware.ts
+++ packages/@eventual/aws-runtime/src/handlers/api/middleware.ts
@@ -1,5 +1,9 @@
 import { MiddlewareObj } from "@middy/core";
 
+/**
+ * A middy middleware to handle crashed api lambdas, emitting the lambda's error in the response body
+ * This gives us visibility into api issues, and is especially useful combined with the cli's debug flag
+ */
 export const errorMiddleware: MiddlewareObj = {
   onError: (req) => {
     return {
