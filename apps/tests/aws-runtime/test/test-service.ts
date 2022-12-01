2f6e3ce24abe6cb101f901cd14da0c117c608e14
Sam Sussman <sussmansa@gmail.com>
2022-12-01 00:10:54 -0800

    more cleanup

--- apps/tests/aws-runtime/test/test-service.ts
+++ apps/tests/aws-runtime/test/test-service.ts
@@ -1,15 +1,10 @@
 import { activity, sleepFor, sleepUntil, workflow } from "@eventual/core";
-import {
-  assertCompleteExecution,
-  waitForWorkflowCompletion,
-  test,
-} from "./runtime-test-harness.js";
 
 const hello = activity("hello", async (name: string) => {
   return `hello ${name}`;
 });
 
-const workflow1 = workflow(
+export const workflow1 = workflow(
   "my-workflow",
   async ({ name }: { name: string }) => {
     const result = await hello(name);
@@ -17,57 +12,17 @@ const workflow1 = workflow(
   }
 );
 
-test("call activity", workflow1, { name: "sam" }, async (executionId) => {
-  const execution = await waitForWorkflowCompletion<typeof workflow1>(
-    executionId
-  );
-
-  assertCompleteExecution(execution);
-
-  expect(execution.result).toEqual("you said hello sam");
-});
-
-const workflow2 = workflow("my-parent-workflow", async () => {
+export const workflow2 = workflow("my-parent-workflow", async () => {
   const result = await workflow1({ name: "sam" });
   return `user: ${result}`;
 });
 
-test("call workflow", workflow2, async (executionId) => {
-  const execution = await waitForWorkflowCompletion<typeof workflow2>(
-    executionId
-  );
-
-  assertCompleteExecution(execution);
-
-  expect(execution.result).toEqual("user: you said hello sam");
-});
-
-const workflow3 = workflow("sleepy", async () => {
+export const workflow3 = workflow("sleepy", async () => {
   await sleepFor(2);
   await sleepUntil(new Date(new Date().getTime() + 1000 * 2));
   return `done!`;
 });
 
-test("sleep", workflow3, async (executionId) => {
-  const execution = await waitForWorkflowCompletion<typeof workflow3>(
-    executionId
-  );
-
-  assertCompleteExecution(execution);
-
-  expect(execution.result).toEqual("done!");
-});
-
-const workflow4 = workflow("parallel", async () => {
+export const workflow4 = workflow("parallel", async () => {
   return Promise.all([hello("sam"), hello("chris"), hello("sam")]);
-});
-
-test("parallel", workflow4, async (executionId) => {
-  const execution = await waitForWorkflowCompletion<typeof workflow4>(
-    executionId
-  );
-
-  assertCompleteExecution(execution);
-
-  expect(execution.result).toEqual(["hello sam", "hello chris", "hello sam"]);
 });
--- apps/tests/aws-runtime/test/tester.test.ts
+++ apps/tests/aws-runtime/test/tester.test.ts
@@ -1,16 +1,23 @@
-import { workflowClient } from "./client-create.js";
-import "./test-service.js";
-import { tests } from "./runtime-test-harness.js";
+import { eventualRuntimeTestHarness } from "./runtime-test-harness.js";
+import { workflow1, workflow2, workflow3, workflow4 } from "./test-service.js";
 
 jest.setTimeout(100 * 1000);
 
-tests.forEach((_test) => {
-  test(_test.name, async () => {
-    const executionId = await workflowClient.startWorkflow({
-      workflowName: _test.workflow.workflowName,
-      input: _test.input,
-    });
+eventualRuntimeTestHarness(({ testCompletion }) => {
+  testCompletion(
+    "call activity",
+    workflow1,
+    { name: "sam" },
+    "you said hello sam"
+  );
 
-    await _test.test(executionId);
-  });
+  testCompletion("call workflow", workflow2, "user: you said hello sam");
+
+  testCompletion("sleep", workflow3, "done!");
+
+  testCompletion("parallel", workflow4, [
+    "hello sam",
+    "hello chris",
+    "hello sam",
+  ]);
 });
