1a683173b9bce74f817e3b0e0b94221fd3c067de
sam <sam@functionless.org>
2022-12-11 17:27:06 +0530

    fix: add DescribeParameters *

--- apps/tests/aws-runtime-cdk/src/app.ts
+++ apps/tests/aws-runtime-cdk/src/app.ts
@@ -1,6 +1,11 @@
 import { App, CfnOutput, CfnResource, Stack } from "aws-cdk-lib";
 import { Queue } from "aws-cdk-lib/aws-sqs";
-import { ArnPrincipal, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import {
+  ArnPrincipal,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as eventual from "@eventual/aws-cdk";
 import path from "path";
@@ -32,6 +37,12 @@ eventual.Service.grantDescribeParameters(stack, role);
 testService.serviceDataSSM.grantRead(role);
 testService.workflows.grantFilterOrchestratorLogs(role);
 testService.activities.grantFilterWorkerLogs(role);
+role.addToPolicy(
+  new PolicyStatement({
+    actions: ["ssm:DescribeParameters"],
+    resources: ["*"],
+  })
+);
 
 const pipeRole = new Role(stack, "pipeRole", {
   assumedBy: new ServicePrincipal("pipes"),
