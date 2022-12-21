c66dd096cffeb817fa050cef2c123841b6c1f4d3
sam <sam@functionless.org>
2022-12-22 02:33:48 +0530

    feat: use esbuild in eventual-infer

--- packages/@eventual/compiler/src/eventual-infer.ts
+++ packages/@eventual/compiler/src/eventual-infer.ts
@@ -4,7 +4,12 @@
  *
  * @see AppSpec
  */
-import { eventSubscriptions, AppSpec } from "@eventual/core";
+import { AppSpec, eventSubscriptions } from "@eventual/core";
+import crypto from "crypto";
+import esbuild from "esbuild";
+import fs from "fs/promises";
+import os from "os";
+import path from "path";
 
 export async function infer() {
   const scriptName = process.argv[2];
@@ -12,7 +17,22 @@ export async function infer() {
     throw new Error(`scriptName undefined`);
   }
 
-  await import(scriptName);
+  const tmp = os.tmpdir();
+
+  const bundle = await esbuild.build({
+    mainFields: ["module", "main"],
+    entryPoints: [scriptName],
+    sourcemap: false,
+    bundle: true,
+    write: false,
+    platform: "node",
+  });
+
+  const script = bundle.outputFiles[0]?.text!;
+  const hash = crypto.createHash("md5").update(script).digest("hex");
+  const scriptFile = path.join(tmp, `${hash}.js`);
+  await fs.writeFile(scriptFile, script);
+  await import(scriptFile);
 
   const eventualData: AppSpec = {
     subscriptions: eventSubscriptions().flatMap((e) => e.subscriptions),
