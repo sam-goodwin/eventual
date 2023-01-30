d5bcfd706f4da29c38bff429daaa5a4b2e319664
sam <sam@functionless.org>
2023-01-30 11:59:51 -0800

    fix tests

--- examples/stock-bot/infra-pulumi/src/index.ts
+++ examples/stock-bot/infra-pulumi/src/index.ts
@@ -2,7 +2,7 @@ import * as eventual from "@eventual/aws-pulumi";
 
 new eventual.Service("StockBot", {
   entry: require.resolve("example-stock-bot-runtime"),
-  name: "stock-bot",
+  name: "stock-bot-pulumi",
   environment: {
     TABLE_NAME: "name",
   },
