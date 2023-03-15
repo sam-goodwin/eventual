99f52cfa403a32df8e5f89182760d2091858a729
Sam Sussman <sussmansa@gmail.com>
2023-03-14 20:15:50 -0500

    fix: log agent should match console.log contract

---
+++ packages/@eventual/core-runtime/test/log-agent.test.ts
@@ -0,0 +1,62 @@
+import { LogLevel } from "@eventual/core";
+import { format } from "util";
+import { DefaultLogFormatter, LogContextType } from "../src/log-agent.js";
+
+describe("formatter", () => {
+  test("object", () => {
+    const obj = { obj: {} };
+    console.log(obj);
+    expect(
+      new DefaultLogFormatter().format({
+        context: { type: LogContextType.Execution, executionId: "" },
+        level: LogLevel.DEBUG,
+        data: [obj],
+        time: 1000,
+      })
+    ).toEqual(`${LogLevel.DEBUG}\t${format(obj)}`);
+  });
+
+  test("string", () => {
+    const val = "some string";
+    console.log(val);
+    expect(
+      new DefaultLogFormatter().format({
+        context: { type: LogContextType.Execution, executionId: "" },
+        level: LogLevel.DEBUG,
+        data: [val],
+        time: 1000,
+      })
+    ).toEqual(`${LogLevel.DEBUG}\t${format(val)}`);
+  });
+
+  test("multiple", () => {
+    const vals = [{ a: "a", B: { c: "c" } }, "some string"];
+    console.log(...vals);
+    expect(
+      new DefaultLogFormatter().format({
+        context: { type: LogContextType.Execution, executionId: "" },
+        level: LogLevel.DEBUG,
+        data: vals,
+        time: 1000,
+      })
+    ).toEqual(`${LogLevel.DEBUG}\t${format(...vals)}`);
+  });
+
+  test("activity", () => {
+    const obj = { obj: {} };
+    console.log(obj);
+    expect(
+      new DefaultLogFormatter().format({
+        context: {
+          type: LogContextType.Activity,
+          activityName: "act",
+          seq: 1,
+          executionId: "",
+        },
+        level: LogLevel.DEBUG,
+        data: [obj],
+        time: 1000,
+      })
+    ).toEqual(`${LogLevel.DEBUG}\tact:1\t${format(obj)}`);
+  });
+});
