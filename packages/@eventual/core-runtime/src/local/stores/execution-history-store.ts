34539c308bd6a0604396f5444b8c807c0e9bc098
Sam Sussman <sussmansa@gmail.com>
2023-03-15 19:27:59 -0500

    add pagination to local history store

--- packages/@eventual/core-runtime/src/local/stores/execution-history-store.ts
+++ packages/@eventual/core-runtime/src/local/stores/execution-history-store.ts
@@ -19,12 +19,50 @@ export class TestExecutionHistoryStore extends ExecutionHistoryStore {
   public async getEvents(
     request: ListExecutionEventsRequest
   ): Promise<ListExecutionEventsResponse> {
+    console.log(request);
+    const tokenPayload = request.nextToken
+      ? deserializeToken(request.nextToken)
+      : undefined;
+    const afterDate = request.after
+      ? new Date(request.after).getTime()
+      : undefined;
     const sortedEvents = (this.eventStore[request.executionId] ?? []).sort(
       (a, b) =>
         request.sortDirection === "ASC"
-          ? new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
-          : new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+          ? new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+          : new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
     );
-    return { events: sortedEvents };
+    const dateFiltered = afterDate
+      ? sortedEvents.filter(
+          (event) => new Date(event.timestamp).getTime() > afterDate
+        )
+      : sortedEvents;
+    const start = tokenPayload?.index ?? 0;
+    const rangeEvents = dateFiltered.slice(
+      start,
+      request.maxResults ? start + request.maxResults : undefined
+    );
+
+    const res = {
+      events: rangeEvents,
+      nextToken:
+        start + rangeEvents.length < dateFiltered.length
+          ? serializeToken({ index: start + rangeEvents.length })
+          : undefined,
+    };
+    console.log(res);
+    return res;
   }
+}
+
+interface TokenPayload {
+  index: number;
+}
+
+function serializeToken(payload: TokenPayload) {
+  return Buffer.from(JSON.stringify(payload)).toString("base64");
+}
+
+function deserializeToken(token: string): TokenPayload {
+  return JSON.parse(Buffer.from(token, "base64").toString("utf-8"));
 }
