3609c41ffa4c5f32a268ff74e8222ccaa121077f
sam <sam@functionless.org>
2023-05-29 18:19:52 -0700

    fix: add request to body

--- packages/@eventual/core/src/search/search-index.ts
+++ packages/@eventual/core/src/search/search-index.ts
@@ -174,7 +174,7 @@ export function index<
       }),
       async (): Promise<Response> => {
         const response = await (index.client as any)[operation]({
-          ...request,
+          body: request,
           index: indexName,
         });
         assertApiResponseOK(response);
