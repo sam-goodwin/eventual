ac12986c39eb9735aa584a5a2e20b8d8a9666b1a
sam <sam@eventual.ai>
2023-09-06 23:32:38 -0700

    wire up

---
+++ packages/@eventual/compiler/bin/eventual-build-service.mjs
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+import { main } from "../lib/cjs/build-service-cli.js";
+
+await main();
--- packages/@eventual/compiler/src/index.ts
+++ packages/@eventual/compiler/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./build-service.js";
 export * from "./eventual-bundle.js";
 export * from "./eventual-infer.js";
 export * from "./build.js";
